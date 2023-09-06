d88350e6700e63a920df198ca7c235b8e635b8a3
张鹏 <scbeta@qq.com>
2023-09-06 14:35:13 +0800

    First commit.

---
+++ Quick.AvaloniaFonts.AlibabaPuHuiTi.Slim/AppBuilderExtension.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia.Media;
+
+namespace Avalonia;
+
+public static class AppBuilderExtension
+{
+    public static AppBuilder WithFont_AlibabaPuHuiTi(this AppBuilder appBuilder)
+    {
+        var uri = "avares://Quick.AvaloniaFonts.AlibabaPuHuiTi.Slim/Assets/Fonts#Alibaba PuHuiTi";
+        return appBuilder.With(new FontManagerOptions()
+        {
+            DefaultFamilyName = uri,
+            FontFallbacks = new[] { new FontFallback { FontFamily = new FontFamily(uri) } }
+        });
+    }
+}
